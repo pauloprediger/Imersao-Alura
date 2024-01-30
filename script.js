d0f24385480e927d4089d2d2c834a9d738d0eb11
Paulo Victor Ribeiro Prediger <142063372+pauloprediger@users.noreply.github.com>
2024-01-30 09:24:26 -0300

    Spotify update

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-//BOM DIA | BOA TARDE | BOA NOITE
 
-// Obtém a referência do elemento com o ID "greeting"
 const greetingElement = document.getElementById("greeting");
 
-// Obtém a hora atual do sistema
+
 const currentHour = new Date().getHours();
 
 
@@ -19,15 +17,10 @@ greetingElement.textContent = greetingMessage;
 // GRID INTELIGENTE
 const container = document.querySelector(".offer__list-item");
 
-const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
-  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
-  const numColumns = Math.floor(containerWidth / 250); //número de colunas com base na largura do container
-
-  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
-  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(250px, 1fr))`;
-
-  console.log({ container });
-  console.log({ numColumns });
+const observer = new ResizeObserver(() => {  
+  const containerWidth = container.offsetWidth; 
+  const numColumns = Math.floor(containerWidth / 240); 
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
 });
-//observando a mudança do elemento
+
 observer.observe(container);
